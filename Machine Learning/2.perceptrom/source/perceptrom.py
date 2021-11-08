fb7692a3154a5fc4ed00e8128ada35915a6bed6e
He1o <1210568288@qq.com>
2021-11-08 23:08:50 +0800

    ''

--- Machine Learning/2.perceptrom/source/perceptrom.py
+++ Machine Learning/2.perceptrom/source/perceptrom.py
@@ -5,13 +5,37 @@ class Perceptron(object):
     def __init__(self, train_data, lable):
         self.train_data = np.array(train_data)
         self.lable = np.array(lable)
-        self.weights = np.append(np.ones((1, 9)), X, axis = 0)
+        self.length = len(train_data)
+        self.train_data = np.append(self.train_data, np.ones((self.length, 1)), axis = 1)
+        self.weights = np.zeros(len(train_data[0]) + 1)
+        self.eta = 1
 
+    def sign(self, x):
+        return 1 if x >= 0 else -1
 
-
-
-    def predict(self, input):
+    def predict(self, input_data):
+        return self.sign(np.sum(self.weights * input_data))
 
 
     def train(self):
+        for _ in range(10):
+            flag = True
+            for i, inpute_data in enumerate(self.train_data):
+                if self.lable[i] * self.predict(inpute_data) <= 0:
+                    self.weights += self.eta * self.lable[i]  * inpute_data
+                    flag = False
+                    plt.plot([0,1], [(-self.weights[2] / self.weights[1]), (-self.weights[2] - self.weights[0]) / self.weights[1]])
+                    plt.pause(3)
+            if flag:
+                return self.weights
 
+train_data = [[2, 3], [1, 4], [3, 5], [2, 6], [4, 5], [3, 1], [4, 3], [6, 2], [2, 1]]
+lable = [1, 1, 1, 1, 1, -1, -1, -1 ,-1]
+
+
+plt.scatter([x[0] for x in train_data[:5]], [x[1] for x in train_data[:5]], c='r')
+plt.scatter([x[0] for x in train_data[5:]], [x[1] for x in train_data[5:]], c='b')
+plt.ion()
+
+ptron = Perceptron(train_data, lable)
+ptron.train()
