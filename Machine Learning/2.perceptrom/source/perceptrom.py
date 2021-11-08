9b4c8645eda4a7a61696db6d54994773261048f1
He1o <1210568288@qq.com>
2021-11-08 17:05:29 +0800

    ’‘

---
+++ Machine Learning/2.perceptrom/source/perceptrom.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Perceptron(object):
+    def __init__(self, train_data, lable):
+        self.train_data = np.array(train_data)
+        self.lable = np.array(lable)
+        self.weights = np.append(np.ones((1, 9)), X, axis = 0)
+
+
+
+
+    def predict(self, input):
+
+
+    def train(self):
+
