feb28ec3c1007459d2ec871de29e87bad0e896fb
He1o <1210568288@qq.com>
2022-08-19 08:18:03 +0800

    ''

---
+++ Kaggle/test.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
+train_data = pd.read_csv('Kaggle/Data/train.csv')
+test_data = pd.read_csv('Kaggle/Data/test.csv')
